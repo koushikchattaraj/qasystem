367024cb3abef2689d44392de4f8140133478250
Koushik <chattarajkoushik@gmail.com>
2021-11-20 00:01:45 +0530

    update serect key

--- qasystem/settings.py
+++ qasystem/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-0!_)wfr1hrpzj822%8#&)4*c8+)w66hx+-*bsx6(^uh7dize(h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
