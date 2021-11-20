5b678e680d6c05a73a0d293d79c2126d487a0c03
Koushik <chattarajkoushik@gmail.com>
2021-11-20 15:58:43 +0530

    add urlspattern

--- qasystem/urls.py
+++ qasystem/urls.py
@@ -12,3 +12,4 @@ urlpatterns = [
     path('', include("votes.urls")),
     path('', include("drive.urls")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
